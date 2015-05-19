87cf97ff525da06b8803a782db41717ae5c87a98
Babneet Singh <babneet.singh@mail.utoronto.ca>
2015-05-18 22:37:45 -0400

    Adding controllers to create a RESTful interface to send data

--- nhoods/controllers/controller1.js
+++ nhoods/controllers/controller1.js
@@ -1,9 +1,70 @@
 var crypto = require('crypto');
 var mongoose = require('mongoose'),
-	location = mongoose.model('Location');
+	Element = mongoose.model('Element');
 
-exports.evaluateLocation = function(req, res) {
+/* Send indicators corresponding to a city [JSON] */
+exports.getIndicators = function(req, res) {
+	var city = req.params.id;
+	Element.find({
+		location: city
+	}).exec(function (err, elements) {
+		if (err) {
+			throw err;
+		} else {
+			var output = [];
+			for (var i in elements) {
+				output[i] = {
+					ind: elements[i].indicator,
+					val: elements[i].value
+				};
+			}
+			res.json(output);
+		}
+	});
+};
 
+/* Send specific indicator info for all cities [JSON] */
+exports.getIndicatorForAllCities = function(req, res) {
+	var indicator = req.params.id;
+	Element.find({
+		indicator: indicator
+	}).exec(function (err, elements) {
+		if (err) {
+			throw err;
+		} else {
+			var output = [];
+			for (var i in elements) {
+				output[i] = {
+					cit: elements[i].location,
+					val: elements[i].value
+				};
+			}
+			res.json(output);
+		}
+	});
+};
+
+/* Send specific indicator for a specific city [JSON] */
+exports.getIndicatorForCity = function(req, res) {
+	var inputs = req.params.id.split("++");
+	var city = inputs[0];
+	var indicator = inputs[1];
+	Element.find({
+		location: city,
+		indicator: indicator
+	}).exec(function (err, elements) {
+		if (err) {
+			throw err;
+		} else {
+			var output = [];
+			for (var i in elements) {
+				output[i] = {
+					val: elements[i].value
+				};
+			}
+			res.json(output);
+		}
+	});
 };
 
 function encryptPassword(password){
