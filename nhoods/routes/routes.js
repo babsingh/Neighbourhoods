621f5906d6114bca312e50ea72893119fbd4151a
Babneet Singh <babneet.singh@mail.utoronto.ca>
2015-05-18 22:38:28 -0400

    Adding routes to support new controllers

--- nhoods/routes/routes.js
+++ nhoods/routes/routes.js
@@ -1,7 +1,12 @@
+var request = require('request');
 var express = require('express');
+var async = require('async');
+
 var router = express.Router();
 
 module.exports = function (app) {
+	var mongoSetup = require('../mongo_setup/get_data');
+	var controller1 = require('../controllers/controller1');
 
 	app.use('/static', express.static( './static'));
 
@@ -9,4 +14,18 @@ module.exports = function (app) {
 	app.get('/', function(req, res, next) {
 	  res.render('index');
 	});
+
+	app.get('/city/:id', controller1.getIndicators);
+
+	app.get('/indicator/:id', controller1.getIndicatorForAllCities);
+
+	app.get('/city/indicator/:id', controller1.getIndicatorForCity);
+
+	//Temporary link for user to update database
+	app.get('/update/database', mongoSetup.getData);
+
+	app.get('/get/cities', mongoSetup.cities);
+
+	app.get('/get/indicators', mongoSetup.indicators);
 }
+
